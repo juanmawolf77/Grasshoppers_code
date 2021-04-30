be74852e80b1fa2fca631b951e4ac8a863f47aef
Abhijeet Shah <abshah@users.noreply.github.com>
2021-04-30 12:24:58 +0200

    Update initial_DE_analysis_script.R
    Made some cosmetic changes. Added a few comments.

--- initial_DE_analysis_script.R
+++ initial_DE_analysis_script.R
@@ -1,16 +1,30 @@
+
+rm(list = ls())
 library("DESeq2")
 library("tximport")
-#IMPORTANTING THE TRANSCRIPT ABUNDANCE DATA
-datakallisto<-list.files("quants_kallisto", pattern = "abundance.tsv", full.names = T, recursive = T) # Creating the vector for the files with a list
-transciptdatakallisto<-lapply(X=datakallisto, FUN = function(x)read.table(x,header = T))  # readig the whole files using a anonymus fuction to then use tximport
+
+
+### Import kallisto pseudo-counts into memory 
+# Creating the vector for the files with a list
+datakallisto<-list.files("quants_kallisto", pattern = "abundance.tsv", full.names = T, recursive = T) 
+
+# readig the whole files using a anonymus fuction to then use tximport
+transciptdatakallisto<-lapply(X=datakallisto, FUN = function(x)read.table(x,header = T))  
+
+# Read in sample meta-data design
 listgrasshopperssamples<-read.table("RNAseq_DE_mapping_samples_list.txt", header = TRUE)
-#reading the data and then generating counts with tximport
+
+# reading the count data with tximport
 countskallisto<-tximport(files=datakallisto , type= "kallisto", txOut =TRUE, countsFromAbundance= "lengthScaledTPM")
+
+# sanity check to see if my column names and row names are OK, also if our data is read into R fine. 
 all(colnames(countskallisto$counts)==row.names(datakallisto))
 View(datakallisto)
 head(datakallisto)
 head(countskallisto)
 str(listgrasshopperssamples)
+
+### creating a object to give to r the information about the design and the information of each sample
 ddsdata<-DESeqDataSetFromTximport(countskallisto, colData =listgrasshopperssamples, design= ~ morph)
 
 summary(ddsdata)
